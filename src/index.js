80b68ec3b3c088e9671266cd0847bf58251ce93c
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-06-26 20:36:03 +0300

    feat: complete reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let result = ''
+    n = String(Math.abs(n))
+    for (i = n.length-1;i>=0;i--) {
+        result = result + n[i]
+    }
+    console.log(result)
+    return result
 }
+
